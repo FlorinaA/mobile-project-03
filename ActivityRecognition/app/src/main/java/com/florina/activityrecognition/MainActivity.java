fe656a1a48072b5713acecc0409c5cfa3e89e6b9
bhoomi15296 <55467168+bhoomi15296@users.noreply.github.com>
2019-10-17 13:18:37 -0400

    Update MainActivity.java

--- ActivityRecognition/app/src/main/java/com/florina/activityrecognition/MainActivity.java
+++ ActivityRecognition/app/src/main/java/com/florina/activityrecognition/MainActivity.java
@@ -9,5 +9,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        acc = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        
+        
     }
 }
