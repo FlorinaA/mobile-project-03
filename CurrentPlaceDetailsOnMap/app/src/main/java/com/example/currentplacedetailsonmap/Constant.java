96fc5a29158bbd48a4dc6a005c2ebd848a4a99ba
Sanika <sanikapatki16@gmail.com>
2019-10-21 00:41:38 -0400

    Maps API integrated with geofencing add, viz

---
+++ CurrentPlaceDetailsOnMap/app/src/main/java/com/example/currentplacedetailsonmap/Constant.java
@@ -0,0 +1,54 @@
+package com.example.currentplacedetailsonmap;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.HashMap;
+
+/**
+ * Constants used in this sample.
+ */
+
+final class Constants {
+
+    private Constants() {
+    }
+
+    private static final String PACKAGE_NAME = "com.google.android.gms.location.Geofence";
+
+    static final String GEOFENCES_ADDED_KEY = PACKAGE_NAME + ".GEOFENCES_ADDED_KEY";
+
+    /**
+     * Used to set an expiration time for a geofence. After this amount of time Location Services
+     * stops tracking the geofence.
+     */
+    private static final long GEOFENCE_EXPIRATION_IN_HOURS = 12;
+
+    /**
+     * For this sample, geofences expire after twelve hours.
+     */
+    static final long GEOFENCE_EXPIRATION_IN_MILLISECONDS =
+            GEOFENCE_EXPIRATION_IN_HOURS * 60 * 60 * 1000;
+    static final float GEOFENCE_RADIUS_IN_METERS = 10; // 1 mile, 1.6 km
+
+    /**
+     * Map for storing information about airports in the San Francisco bay area.
+     */
+    static final HashMap<String, LatLng> BAY_AREA_LANDMARKS = new HashMap<>();
+
+    static {
+        // San Francisco International Airport.
+        BAY_AREA_LANDMARKS.put("SFO", new LatLng(37.621313, -122.378955));
+
+        // Googleplex.
+        BAY_AREA_LANDMARKS.put("GOOGLE", new LatLng(37.422611,-122.0840577));
+
+        // 37 William Street
+        BAY_AREA_LANDMARKS.put("37-William", new LatLng(42.267241, -71.807712));
+
+        // Fuller labs
+//        BAY_AREA_LANDMARKS.put("Fuller", new LatLng(42.274997, -71.806383));
+
+        // Gordon Library
+//        BAY_AREA_LANDMARKS.put("Library", new LatLng(42.274215, -71.806393));
+    }
+}
---
+++ CurrentPlaceDetailsOnMap/app/src/main/java/com/example/currentplacedetailsonmap/GeofenceTransitionsJobIntentService.java
@@ -0,0 +1,34 @@
+package com.example.currentplacedetailsonmap;
+
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.os.Build;
+//import androidx.core.app.JobIntentService;
+//import androidx.core.app.NotificationCompat;
+//import androidx.core.app.TaskStackBuilder;
+import android.text.TextUtils;
+import android.util.Log;
+
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingEvent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Listener for geofence transition changes.
+ *
+ * Receives geofence transition events from Location Services in the form of an Intent containing
+ * the transition type and geofence id(s) that triggered the transition. Creates a notification
+ * as the output.
+ */
+public class GeofenceTransitionsJobIntentService  {
+
+
+}
