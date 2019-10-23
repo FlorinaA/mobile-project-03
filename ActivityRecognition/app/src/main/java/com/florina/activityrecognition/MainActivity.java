bfbc9a99ea8cedb2a6de7234daa4adfc1ef560d5
FlorinaAsani <florina.asani@live.com>
2019-10-23 19:12:26 -0400

    Initial setup using service instead of broadcast

--- ActivityRecognition/app/src/main/java/com/florina/activityrecognition/MainActivity.java
+++ ActivityRecognition/app/src/main/java/com/florina/activityrecognition/MainActivity.java
@@ -1,7 +1,20 @@
 package com.florina.activityrecognition;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.location.ActivityRecognition;
+import com.google.android.gms.location.ActivityRecognitionClient;
+import com.google.android.gms.location.ActivityTransition;
+import com.google.android.gms.location.ActivityTransitionRequest;
+import com.google.android.gms.location.DetectedActivity;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -10,6 +23,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener, StepListener {
     private TextView textView;
@@ -18,6 +35,13 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private Sensor accel;
     private int numSteps;
     private TextView TvSteps;
+
+//    Activity Recognition
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private List<ActivityTransition> transitions;
+    private ActivityRecognitionClient activityRecognitionClient;
+    private PendingIntent transitionPendingIntent;
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +55,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         TvSteps = (TextView) findViewById(R.id.tv_steps);
         numSteps = -1;
         sensorManager.registerListener(MainActivity.this, accel, SensorManager.SENSOR_DELAY_FASTEST);
+//        ACTIVITY RECOGNITION
+        mContext = this;
+        activityRecognitionClient = ActivityRecognition.getClient(mContext);
+
+        Intent intent = new Intent(this, TransitionIntentService.class);
+        transitionPendingIntent = PendingIntent.getService(this, 100, intent, PendingIntent.FLAG_UPDATE_CURRENT);
 
     }
 
@@ -50,6 +80,73 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     public void step(long timeNs) {
         numSteps++;
         TvSteps.setText("steps taken " + numSteps);
+    }
+
+    public void registerHandler(View view) {
+        transitions = new ArrayList<>();
+
+        transitions.add(new ActivityTransition.Builder()
+                .setActivityType(DetectedActivity.WALKING)
+                .setActivityTransition(ActivityTransition.ACTIVITY_TRANSITION_ENTER)
+                .build());
+
+
+        transitions.add(new ActivityTransition.Builder()
+                .setActivityType(DetectedActivity.WALKING)
+                .setActivityTransition(ActivityTransition.ACTIVITY_TRANSITION_EXIT)
+                .build());
+
+
+        transitions.add(new ActivityTransition.Builder()
+                .setActivityType(DetectedActivity.RUNNING)
+                .setActivityTransition(ActivityTransition.ACTIVITY_TRANSITION_ENTER)
+                .build());
+
+        transitions.add(new ActivityTransition.Builder()
+                .setActivityType(DetectedActivity.RUNNING)
+                .setActivityTransition(ActivityTransition.ACTIVITY_TRANSITION_EXIT)
+                .build());
+
+
+        ActivityTransitionRequest activityTransitionRequest = new ActivityTransitionRequest(transitions);
+
+        Task<Void> task = activityRecognitionClient.requestActivityTransitionUpdates(activityTransitionRequest, transitionPendingIntent);
+
+        task.addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Toast.makeText(mContext, "Transition update set up", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        task.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(mContext, "Transition update Failed to set up", Toast.LENGTH_LONG).show();
+                e.printStackTrace();
+            }
+        });
+
+    }
+
+
+    public void deregisterHandler(View view) {
+        Task<Void> task = activityRecognitionClient.removeActivityTransitionUpdates(transitionPendingIntent);
+        task.addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                transitionPendingIntent.cancel();
+                Toast.makeText(mContext, "Remove Activity Transition Successfully", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        task.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(mContext, "Remove Activity Transition Failed", Toast.LENGTH_LONG).show();
+                e.printStackTrace();
+            }
+        });
     }
 
 }
